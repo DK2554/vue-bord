b9e213102b2efed126b6f3f0e4b67ad7101a6590
DK2554 <ldu0215@gmail.com>
2021-02-09 22:24:11 +0900

    Initial commit

---
+++ src/test/java/com/spring/vuebord/VueBordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vuebord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
